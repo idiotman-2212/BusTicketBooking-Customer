8c01a14c15fef24605e83dfc1d73169e554a3bb1
ChauHuyDien <82036270+idiotman-2212@users.noreply.github.com>
2024-08-04 22:33:59 +0700

    first commit

---
+++ src/utils/http.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const http = axios.create({
+    //VPS
+    baseURL: 'http://localhost:8080/api/v1/',
+    timeout: 10000, // 10s
+    headers: {
+        "Content-Type": "application/json",
+    }
+})
+
+http.interceptors.request.use((config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);   
+})
+
+export { http }
